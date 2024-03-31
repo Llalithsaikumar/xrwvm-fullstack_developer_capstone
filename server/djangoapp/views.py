29e39393ea8185a21dfa489dc6de3d94a1532982
L Lalith Sai Kumar <107413674+Llalithsaikumar@users.noreply.github.com>
2024-03-31 18:08:01 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -86,7 +86,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200, "dealers": dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
@@ -98,14 +98,13 @@ def get_dealer_reviews(request, dealer_id):
         if reviews is None:
             return JsonResponse({"status": 500, "message":
                                  "Error fetching reviews"})
-        
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             if response is None:
                 review_detail['sentiment'] = "unknown"
             else:
-                review_detail['sentiment'] = response.get('sentiment', "unknown")
-        
+                review_detail['sentiment'] = response.get('sentiment',
+                                                          "unknown") 
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
@@ -116,9 +115,10 @@ def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200, "dealer": dealership})
+        return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
 def add_review(request):
