8cd10f26304cdd0dfc266f62fd7459eb014af669
L Lalith Sai Kumar <107413674+Llalithsaikumar@users.noreply.github.com>
2024-03-31 18:15:19 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments,
 
 
 # Get an instance of a logger
@@ -123,7 +123,6 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if not request.user.is_anonymous:
-        data = json.loads(request.body)
         try:
             return JsonResponse({"status": 200})
         except Exception as e:
